ac0bad23fba89924f054957fa918b1e4696d692c
Alexandre Resende <aar6947@gmail.com>
2017-10-15 21:42:03 -0200

    refactor: Move routes files to server folder

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const fs = require('fs');
 
-const helloRouter = require(__dirname + '/../routes/hello.route');
+const helloRouter = require(__dirname + '/routes/hello.route');
 const configPort = require(__dirname + '/../config.json').port;
 
 const app = express();
